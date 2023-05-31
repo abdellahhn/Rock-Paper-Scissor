03ae78dfdef17370219c4a9115ad9ab51018532c
abdellahhn <abdellahhanane44@gmail.com>
2023-05-31 11:05:18 -0400

    final

--- game.js
+++ game.js
@@ -7,82 +7,65 @@ function getComputerChoice(){
 
 console.log(getComputerChoice());
 
-// function roundPlay(player_Choice, computer_choice){
-//     const selectionLower = player_Choice.toLowerCase();
-//     if (selectionLower === 'rock'){
-//         if (computer_choice === 'scissor'){
-//             return 'win!';
-//         } else if (computer_choice === 'paper'){
-//             return 'lose!';
-//         } else {
-//             return 'tie';
-//         }
-//     } else if (selectionLower === 'scissor'){
-//         if (computer_choice === 'rock'){
-//             return 'lose!';
-//         } else if (computer_choice === 'paper'){
-//             return 'win!';
-//         } else {
-//             return 'tie';
-//         }
-//     } else if (selectionLower === 'paper'){
-//         if (computer_choice === 'rock'){
-//             return 'win!';
-//         } else if (computer_choice === 'scissor'){
-//             return 'lose!';
-//         } else {
-//             return 'tie';
-//         }
-//     }  else {
-//     return "Invalid selection. Please choose rock, paper, or scissors.";
-//     }
-// }
-//
+function roundPlay(player_Choice, computer_choice){
+    player_Choice = player_Choice.toLowerCase();
+    if (
+        (player_Choice === 'rock' && computer_choice === 'scissor') ||
+        (player_Choice === 'scissor' && computer_choice === 'paper') ||
+        (player_Choice === 'paper' && computer_choice === 'rock')) {
+        return 'win!';
+    } else if(
+        (player_Choice === 'scissor' && computer_choice === 'rock') ||
+        (player_Choice === 'paper' && computer_choice === 'scissor') ||
+        (player_Choice === 'rock' && computer_choice === 'paper')
+    ) {
+        return "Lose!";
+    } else {
+        return "Tie!";
+    }
+}
+
 // const playerSelection = "rock";
 // const computerSelection = getComputerChoice();
 // console.log(roundPlay(playerSelection, computerSelection));
 
 function game(){
+    const prompt = require("prompt-sync")({sigint:true});
+    let pt_computer = 0;
+    let pt_player = 0;
 
 
-    let count = 5;
+    for (let round = 1; round <= 5; round++){
+        console.log(`Round ${round}:`);
+        let player_choice = prompt("Enter your choice: ");
+        let computer_choice = getComputerChoice();
 
-    for (let i = count; i>=0; i--){
-        const prompt = require("prompt-sync")({sigint:true});
-        let playerChoice = prompt("Enter your choice: ");
-        const computerSelection = getComputerChoice();
+        console.log(`Player choice: ${player_choice}`);
+        console.log(`Computer choice: ${computer_choice}`);
 
-        console.log(playerChoice, computerSelection);
-
-        const selectionLower = playerChoice.toLowerCase();
-        if (selectionLower === 'rock'){
-            if (computerSelection === 'scissor'){
-                console.log('Win!');
-            } else if (computerSelection === 'paper'){
-                console.log('lose!');
-            } else {
-                console.log('Tie!');
-            }
-        } else if (selectionLower === 'scissor'){
-            if (computerSelection === 'rock'){
-                console.log('Lose');
-            } else if (computerSelection === 'paper'){
-                console.log('Win!!');
-            } else {
-                console.log('tie!');
-            }
-        } else if (selectionLower === 'paper'){
-            if (computerSelection === 'rock'){
-                console.log('Win!');
-            } else if (computerSelection === 'scissor'){
-                console.log('Lose');
-            } else {
-                console.log('Tie!');
-            }
-        }  else {
-            console.log("Invalid selection. Please choose rock, paper, or scissors.");
+        let round_res = roundPlay(player_choice, computer_choice);
+        if (round_res === 'win!'){
+            console.log('Round Won!');
+            pt_player ++;
+        } else if (round_res === 'Lose!'){
+            console.log('Round Lost!');
+            pt_computer ++;
+        } else {
+            console.log("Tie!");
         }
+    }
+
+    console.log("Game Over!!");
+    console.log(`Player Score: ${pt_player}`);
+    console.log(`Computer Score: ${pt_computer}`);
+
+    if (pt_player > pt_computer){
+        console.log("You Win!");
+    } else if (pt_player < pt_computer){
+        console.log("You Lose!");
+    } else {
+        console.log("Tie!")
     }
 }
 
-console.log(game());
+game();
